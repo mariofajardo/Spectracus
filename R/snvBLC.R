1f71fd9cf3fb6cd50583eca10760ae63234423c6
Mario Fajardo <mario.fajardopedraza@sydney.edu.au>
2015-03-23 15:24:19 +1100

    snvBLC function optimized

--- R/snvBLC.R
+++ R/snvBLC.R
@@ -1,12 +1,12 @@
 #' Standard Normal Variate (SNV) transformation
 #' 
 #' @param spectra a matrix or data.frame with wavelengths as columns and spectra as rows
+#' @importFrom plyr aaply
 #' @export
 
 snvBLC<- function(spectra){
   spectra<-as.matrix(spectra)
-  snvMat<- matrix(NA,ncol=ncol(spectra),nrow=nrow(spectra))
-  for (i in 1: nrow(spectra)){
-    snvMat[i,]<-(spectra[i,]- mean(spectra[i,]))/ sd(spectra[i,])}
-  colnames(snvMat) <- colnames(spectra)
-  return(snvMat)}
+  snvMat<-(spectra - rowMeans(spectra))/apply(spectra,1,sd)
+  attributes(snvMat) <- attributes(spectra)
+  snvMat
+}
