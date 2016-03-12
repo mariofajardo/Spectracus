53383575ed91d55726b059cc84ec112cab2ed8a5
Mario Fajardo <mario.fajardopedraza@sydney.edu.au>
2016-03-12 19:36:36 +1100

    compspec example added

--- R/compSpec.R
+++ R/compSpec.R
@@ -6,10 +6,24 @@
 #' @param spectra matrix where each row is a spectrum and each column a wavelength
 #' @param window window size over which the spectra will be averaged
 #' @examples
-#' observed <- c(1,5,6,8,9,20)
-#' predicted <- c(2,5,6,7,9,22)
-#' goof(observed,predicted)
-#' goof(observed,predicted,coefficient = c('R2','RMSE','RPD'),cex=3,pch=19,col='blue')
+#'#load data
+#'data("soilSpecDat")
+#'
+#'#subset spectra
+#'spectra <- soilSpecDat[,grepl('[X]',colnames(soilSpecDat))]
+#'colnames(spectra) <- substr(colnames(spectra),2,5)
+#'wavelengths <- as.numeric(colnames(spectra))
+#'
+#'#plot spectrum
+#'plot(wavelengths,spectra[1,],type='b')
+#'
+#'spectra_2 <- compSpec(spectra,9)
+#'wavelength_2 <- colnames(spectra_2)
+#'
+#'#plot processed spectrum
+#'points(wavelength_2,spectra_2[1,],cex=2,col='red',pch=21,bg='blue') 
+#'
+#'
 #' @author Brendan Malone
 #' @export
 
