9a9b5b9d87283aa6ee10f78b586f70df34298005
Mario Fajardo Pedraza <mario.fajardo@sydney.edu.au>
2017-12-07 14:12:52 +1100

    Multiple filters option added in filter_SoilSpectra method

--- R/filter_SoilSpectra.R
+++ R/filter_SoilSpectra.R
@@ -15,6 +15,8 @@
 #' @param If type = 'S-Golay' then n,p,m parameters control \code{\link{sgolayfilt}} window size, polynomial order and derivative order respectively.
 #' @param If type = 'Wavelet' res level to be extracted from wavelet decomposition model, see \code{\link{accessC.wd}}
 #' @param If type = 'C-hull' specType also needs to be specified, with 0 if the data is in absorbance units or 1 if the data is in reflectance units.
+#' @param If type is a character vector with more than one filter type the method will apply all the filters in the order they are specified.
+#' @importFrom signal sgolayfilt
 #' @importFrom signal sgolayfilt
 #' @importFrom wavethresh wd accessC.wd
 #' @references Savitzky, Abraham and Golay, M. J. E. (1964) Smoothing and Differentiation of Data by Simplified Least Squares Procedures, Analytical Chemistry, 36, pp. 1627-1629.
@@ -90,125 +92,132 @@ setMethod(f = 'filter_SoilSpectra',
     spectra <- SoilSpectra@Spectra
     specType <- SoilSpectra@Type
     
-  if (type =='S-Golay'){
-    ## run filter
-    sg <- t(apply(spectra, 1, sgolayfilt, n = n, p = p, m = m))
-    SoilSpectra@Spectra <- sg
-    SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','S-Golay','n=',n,'p=',p,'m=',m)
-    return(SoilSpectra)
-  }
-
-  if(type=='MSC') {
-    #first calculate a mean spectrum.
-    meanSpec <- colMeans(spectra)
-    mscMat <- matrix(NA, ncol = ncol(spectra), nrow = nrow(spectra))
-    for (i in 1:nrow(spectra)) {
-      # determine the slope and intercept co-efficents
-      specLM <- lm(spectra[i, ] ~ meanSpec)
-      specCE <- t(as.matrix(specLM$coefficients))
-      # Adjust the spectra
-      mscMat[i, ] <- t(as.matrix((spectra[i, ] - specCE[1, 1])/specCE[1,2]))
+    if(length(type)>1){
+      for(treatment in type){
+        SoilSpectra <- filter_SoilSpectra(SoilSpectra,treatment)
       }
-    
-    SoilSpectra@Spectra <- mscMat
-    SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','MSC')
-    return(SoilSpectra)
-    }
-             
-  if(type=='SNV'){
-    snvMat<-(spectra - rowMeans(spectra))/apply(spectra,1,sd)
-    SoilSpectra@Spectra <- snvMat
-    SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','SNV')
-    return(SoilSpectra)
-  }
-  
-  if(type=='Wavelet'){
-    nm2<- 2^c(1:100)
-    vs<- ncol(spectra)
-    if (sum(nm2 == vs) != 1) {
-      stop("Error: Number of columns in spectra table needs to equal 2^x")
+      return(SoilSpectra)
+      
     }else{
-      wave_spectra <- matrix(NA, ncol = 2^res, nrow = nrow(spectra))
       
-     for (i in 1:nrow(spectra)){
-       wds <- wd(as.matrix(spectra[i, ]), 
-                 bc = "symmetric", 
-                 filter.number = 10, 
-                 family = "DaubExPhase", 
-                 min.scale = 2)
-       
-       wave_spectra[i, ] <- accessC.wd(wds, level = res)
-     }
-      
-      wave_spectra<- as.data.frame(wave_spectra)
-      colnames(wave_spectra) <- seq((as.numeric(names(spectra)[1]) + 0.5 * (ncol(spectra)/(2^res))), as.numeric(names(spectra)[length(spectra)]), by = ncol(spectra)/(2^res)) 
-    }
-      return(wave_spectra)
-  }
-    
-  if(type=='C-hull'){
-    c_hull.fix<- function(c_hull){
-      cc<-c_hull
-      xs<-which(cc == 1)
-      ccd<- sort(cc[which(cc == 1):length(cc)])
-      if (cc[1]< cc[length(cc)]){
-        ccd<-ccd
-        }else{
-          
-         for(f in 1:(xs-1)){
-           if(cc[f]< cc[f+1]){
-             ccd<- c(ccd,cc[f])
-            }else{
-            ccd<- c(ccd,cc[f])
-            break}
-         }
-        }
-    return(ccd)
+      if (type =='S-Golay'){
+        ## run filter
+        sg <- t(apply(spectra, 1, sgolayfilt, n = n, p = p, m = m))
+        SoilSpectra@Spectra <- sg
+        SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','S-Golay','n=',n,'p=',p,'m=',m)
+        return(SoilSpectra)
       }
-    interval<- c(1:ncol(spectra))
-    hull_spectra<- matrix(NA,ncol=ncol(spectra),nrow=nrow(spectra))
-    
-    for (i in 1:nrow(spectra)){
-      tempSpect= as.matrix(spectra[i,])
-      data1 <- sortedXyData(interval, tempSpect)
       
-      ## calculate convex hull
-      c_hull <- chull(data1)
+      if(type=='MSC') {
+        #first calculate a mean spectrum.
+        meanSpec <- colMeans(spectra)
+        mscMat <- matrix(NA, ncol = ncol(spectra), nrow = nrow(spectra))
+        for (i in 1:nrow(spectra)) {
+          # determine the slope and intercept co-efficents
+          specLM <- lm(spectra[i, ] ~ meanSpec)
+          specCE <- t(as.matrix(specLM$coefficients))
+          # Adjust the spectra
+          mscMat[i, ] <- t(as.matrix((spectra[i, ] - specCE[1, 1])/specCE[1,2]))
+        }
+        
+        SoilSpectra@Spectra <- mscMat
+        SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','MSC')
+        return(SoilSpectra)
+      }
       
-      ## get the appropriate region
-      cc<-c_hull
-      xs<-which(cc == 1)
-      ccd<- sort(cc[which(cc == 1):length(cc)])
-      if (cc[1]< cc[length(cc)]){
-        ccd<-ccd
+      if(type=='SNV'){
+        snvMat<-(spectra - rowMeans(spectra))/apply(spectra,1,sd)
+        SoilSpectra@Spectra <- snvMat
+        SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','SNV')
+        return(SoilSpectra)
+      }
+      
+      if(type=='Wavelet'){
+        nm2<- 2^c(1:100)
+        vs<- ncol(spectra)
+        if (sum(nm2 == vs) != 1) {
+          stop("Error: Number of columns in spectra table needs to equal 2^x")
         }else{
+          wave_spectra <- matrix(NA, ncol = 2^res, nrow = nrow(spectra))
           
-          for(f in 1:(xs-1)){
-            if(cc[f]< cc[f+1]){
-              ccd<- c(ccd,cc[f])
+          for (i in 1:nrow(spectra)){
+            wds <- wd(as.matrix(spectra[i, ]), 
+                      bc = "symmetric", 
+                      filter.number = 10, 
+                      family = "DaubExPhase", 
+                      min.scale = 2)
+            
+            wave_spectra[i, ] <- accessC.wd(wds, level = res)
+          }
+          
+          wave_spectra<- as.data.frame(wave_spectra)
+          colnames(wave_spectra) <- seq((as.numeric(names(spectra)[1]) + 0.5 * (ncol(spectra)/(2^res))), as.numeric(names(spectra)[length(spectra)]), by = ncol(spectra)/(2^res)) 
+        }
+        return(wave_spectra)
+      }
+      
+      if(type=='C-hull'){
+        c_hull.fix<- function(c_hull){
+          cc<-c_hull
+          xs<-which(cc == 1)
+          ccd<- sort(cc[which(cc == 1):length(cc)])
+          if (cc[1]< cc[length(cc)]){
+            ccd<-ccd
+          }else{
+            
+            for(f in 1:(xs-1)){
+              if(cc[f]< cc[f+1]){
+                ccd<- c(ccd,cc[f])
               }else{
                 ccd<- c(ccd,cc[f])
                 break}
+            }
           }
-          }
-        c_hull <- ccd
+          return(ccd)
+        }
+        interval<- c(1:ncol(spectra))
+        hull_spectra<- matrix(NA,ncol=ncol(spectra),nrow=nrow(spectra))
         
-        ## calculate linear approximation between hull points
-        linear_approx <- approx(data1[c_hull,], xout = interval, method = 'linear', ties = 'mean')
-        ## calculate the deviation from the convex hull
-        if (SoilSpectra@Type=='Absorbance')
-        {hull_spectra[i,] <- 1- (( linear_approx[[2]] - tempSpect )/linear_approx[[2]])}
-        if (SoilSpectra@Type=='Reflectance')
-        {hull_spectra[i,] <- (( linear_approx[[2]] - tempSpect )/linear_approx[[2]])}
-    }
-    
-    SoilSpectra@Spectra <- hull_spectra
-    SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','C-Hull')
-    return(SoilSpectra)
-    }
-    
-    
-    
+        for (i in 1:nrow(spectra)){
+          tempSpect= as.matrix(spectra[i,])
+          data1 <- sortedXyData(interval, tempSpect)
+          
+          ## calculate convex hull
+          c_hull <- chull(data1)
+          
+          ## get the appropriate region
+          cc<-c_hull
+          xs<-which(cc == 1)
+          ccd<- sort(cc[which(cc == 1):length(cc)])
+          if (cc[1]< cc[length(cc)]){
+            ccd<-ccd
+          }else{
+            
+            for(f in 1:(xs-1)){
+              if(cc[f]< cc[f+1]){
+                ccd<- c(ccd,cc[f])
+              }else{
+                ccd<- c(ccd,cc[f])
+                break}
+            }
+          }
+          c_hull <- ccd
+          
+          ## calculate linear approximation between hull points
+          linear_approx <- approx(data1[c_hull,], xout = interval, method = 'linear', ties = 'mean')
+          ## calculate the deviation from the convex hull
+          if (SoilSpectra@Type=='Absorbance')
+          {hull_spectra[i,] <- 1- (( linear_approx[[2]] - tempSpect )/linear_approx[[2]])}
+          if (SoilSpectra@Type=='Reflectance')
+          {hull_spectra[i,] <- (( linear_approx[[2]] - tempSpect )/linear_approx[[2]])}
+        }
+        
+        SoilSpectra@Spectra <- hull_spectra
+        SoilSpectra@Treatments <- paste0(SoilSpectra@Treatments,' + ','C-Hull')
+        return(SoilSpectra)
+      }
+        
+      }
 
           }
 ) 
