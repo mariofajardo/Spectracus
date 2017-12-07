5c5464c37463a81ceb391492cb0d76428b152eaf
Mario Fajardo Pedraza <mario.fajardo@sydney.edu.au>
2017-12-07 12:18:17 +1100

    plot method added

---
+++ R/plot.R
@@ -0,0 +1,27 @@
+#' plot method for SoilSpectra objects
+#' 
+#' This method generates a plot for a Spectra Object
+#' @rdname  plot
+#' @param SoilSpectra Object of class \code{\link{SoilSpectra}}.
+#' @author Mario Fajardo.
+#' @exportMethod   plot
+#' @examples  
+#' \dontrun{
+#' data("SoilSpectraExample")
+#' plot(SoilSpectraExample)
+#' plot(SoilSpectraExample,type='l')
+#' }
+
+
+setMethod(f = 'plot',
+          signature(x='SoilSpectra'),
+          definition= function(x,...)
+          {
+            plot(x@Wavelength,
+                x@Spectra[1,],
+                ylab=x@Type,
+                xlab=slotNames(x)[3],
+                ...)
+          }
+) 
+
